aa8150f52389671f042dc9a70082d32b3fcaaaa4
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2020-02-03 18:45:44 +0000

    Comments added / Changes made to sql code

--- dbms.sql
+++ dbms.sql
@@ -1,14 +1,21 @@
 Relational Schema-
 
+Employee (ID (PK), BaseID (FK), ManagerID)
 Booking (BookID (PK), StartDate, EndDate, Insurance, Day, VehicleID (FK))
 Client (LicenseID (PK), FirstName, LastName, DOB, Age, AddressID (FK),Mobile, Email)
+Bind (ClientID (FK), Type, Age)
 Drivers(LicenseID (PK), BookID (pk))
 Address (ID (PK), StreetName, Town, City, PostCode)
-Vehicle (Name, ID (PK), Seats, Engine, Cost, Type, Transmission, BaseID (FK))
+Vehicle (Name, ID (PK), Seats, Engine, Cost, Type, Transmission, Availability, BaseID (FK))
 Base (BaseID (PK), AddressID (FK))
-Service (Availability, Schedule)
 Cost (Price, Discount, BookID (FK))
 
+CREATE TABLE Employee (
+
+  ID INT PRIMARY KEY AUTO_INCREMENT,
+  BaseID TINYINT FOREIGN KEY REFERENCES Base(ID),
+  ManagerID INT
+) ENGINE=INNODB;
 
 CREATE TABLE Booking (
 
@@ -30,7 +37,17 @@ Age TINYINT CHECK (Age>=25),
 AddressID BIGINT FOREIGN KEY REFERENCES Address(ID)
 Mobile CHAR(10) UNIQUE,
 Email VARCHAR(50) UNIQUE
-) ENGINE=INNODB
+) ENGINE=INNODB;
+
+CREATE TABLE Bind (
+  ClientID VARCHAR(20),
+  PRIMARY KEY (ClientID),
+  FOREIGN KEY (ClientID) REFERENCES Client(LicenseID),
+  Type VARCHAR(50) ,
+  Age TINYINT,
+  CONSTRAINT
+) ENGINE=INNODB;
+
 
 CREATE TABLE Address (
 ID BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -38,7 +55,7 @@ StreetName VARCHAR(50) NOT NULL,
 Town VARCHAR(20) NOT NULL,
 City VARCHAR(20) NOT NULL,
 PostCode VARCHAR(12) NOT NULL
-) ENGINE=INNODB
+) ENGINE=INNODB;
 
 CREATE TABLE Vehicle (
 ID BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -47,30 +64,75 @@ Seats TINYINT DEFAULT 4,
 Engine VARCHAR(20) NOT NULL,
 Cost TINYINT(100) NOT NULL,
 Type VARCHAR(50) NOT NULL,
+Availability BOOLEAN,
 Transmission VARCHAR(50) DEFAULT 'Automatic' CHECK (Transmission='Automatic' OR Transmission='Manual'),
 BaseID TINYINT FOREIGN KEY REFERENCES Base(ID)
-) ENGINE=INNODB
+) ENGINE=INNODB;
 
 CREATE TABLE Base (
 ID TINYINT PRIMARY KEY AUTO_INCREMENT,
 AddressID BIGINT FOREIGN KEY REFERENCES Address(ID)
-) ENGINE=INNODB
+) ENGINE=INNODB;
 
 CREATE TABLE Cost (
 Discount INT,
 Price INT,
 BookID VARCHAR(50) FOREIGN KEY REFERENCES Booking(ID),
 Primary KEY(BookID, Price)
-) ENGINE=INNODB
+) ENGINE=INNODB;
 
 CREATE TABLE Drivers (
 LicenseID VARCHAR(20) FOREIGN KEY REFERENCES Client(LicenseID),
 BookID VARCHAR(50) FOREIGN KEY REFERENCES Booking(ID),
 Primary KEY(BookID, LicenseID)
-) ENGINE=INNODB
+) ENGINE=INNODB;
 
-CREATE TABLE Service (
 
-Availability BOOLEAN,
-Schedule DATE,
-)ENGINE=INNODB
+/*
+**Employee**
+Employee class is created to allow us to have recursive relation.
+A manager supervises an employee, who is also an employee, which
+makes up the recursive relation. It has many to one relationship
+with base.
+
+**Base**
+Base has one to one relationship with Address Entity.
+
+**Vehicle**
+Vehicle has its necessary specifications and many to one
+relationship with Base entity.
+
+**Booking**
+This entity holds booking details from any booking made by a client.
+Therefore, it has many to one relationship. There is also an
+associated class (Cost) that decides the cost of the booking in the
+booking to client relation. It has the references to the vehicle with
+many to one relation.
+
+The price and discount columns are calculated using the cost column from the vehicle table and
+the days column from the booking table,
+Which calculates the cost of booking the vehcle for the required amount of days
+but if the client books the vehicle for > 7 days a discount of 20% will be applicable and will be automatically cut from the price.
+
+**Address**
+Stores the address of clients and bases, has a one-to-one relationship with client
+and base tables.
+
+**Drivers**
+This the entity that holds the additional drivers from the booking.
+Therefore, it has many to one relationship with the booking.
+
+**Client**
+It holds the information of a client.
+
+
+**Bind**
+This class will determine the type of cars that a person can have according
+to their age restriction.
+
+Age works around a check function which uses the datediff() function to get the current date with getdate()
+and the date given in the DOB column in Client to get the age of the driver.
+This in turn allows us to check and apply a constraint stating that if the age of the client is above 25
+but below 30 the car types they are allowed to take is restricted whereas if there age is above 30
+they can take any car type.
+*/
