226f59ca74e5b327f46cef4e212971d9c6cab6fc
Kapadia <hk42@hw.ac.uk>
2020-02-08 18:18:14 +0000

    Sql Updated and report added

--- dbms.sql
+++ dbms.sql
@@ -1,16 +1,20 @@
-/* Relational Schema-
+/* 
+T2- Relational Schema-
 
 Employee (SSN (PK), FirstName, LastName, DOB, BaseID (FK), ManagerSSN)
-Booking (BookID (PK), SSN, StartDate, EndDate, Insurance, Day, VehicleID (FK))
-Client (SSN (PK), FirstName, LastName, DOB, Age, AddressID (FK),Mobile, Email)
-Bind (ClientID (FK), VehicleID (FK),Type, Age)
-Drivers(SSN (PK), BookID (pk))
+Booking (BookID (PK), SSN, StartDate, EndDate, Insurance, /Day, VehicleID (FK))
+Client (SSN (PK), FirstName, LastName, DOB, /Age, AddressID (FK), Mobile, Email)
+Bind (ClientID (FK), VehicleID (FK), Type, Age)
+Drivers (SSN (PK), BookID (pk))
 Address (ID (PK), StreetName, Town, City, PostCode)
 Vehicle (Name, ID (PK), Seats, Engine, Cost, Type, Transmission, Availability, BaseID (FK))
 Base (BaseID (PK), AddressID (FK))
 Cost (Price, Discount, BookID (FK)) 
 */
 
+/*
+T3- MySQL Schema (With Commments)
+*/
 
 CREATE TABLE Address (
 
@@ -103,18 +107,7 @@ CREATE TABLE Drivers (
   FOREIGN KEY (BookID) REFERENCES Booking(ID),
   Primary KEY(BookID, SSN)
 ) ENGINE=INNODB;
-
-/*
-
-T4 Indexes-
-
-CREATE INDEX EmployeeID ON Employee(FirstName, LastName, BaseID);
-CREATE INDEX BookingDetails ON Booking(SSN, VehicleID, Day);
-CREATE INDEX VehicleID ON Vehicle(Name, Type, Availability, Cost);
-CREATE INDEX ClientDetails ON Client(FirstName,LastName,Age,Mobile,Email);
-CREATE INDEX Address ON Address(StreetName, City, PostCode);
-
-*/
+/
 
 /*
 **Employee**
@@ -133,14 +126,18 @@ relationship with Base entity.
 **Booking**
 This entity holds booking details from any booking made by a client.
 Therefore, it has many to one relationship. There is also an
-associated class (Cost) that decides the cost of the booking in the
-booking to client relation. It has the references to the vehicle with
+associated class (Cost) that decides the cost of the booking after discount
+in the booking to client relation. It has the references to the vehicle with
 many to one relation.
+
+The Day attribute is a derived attribute calculated from the StartDate & Enddate attributes
+which is calculated with (EndDate - StartDate) 
 
 The price and discount columns are calculated using the cost column from the vehicle table and
 the days column from the booking table,
 Which calculates the cost of booking the vehcle for the required amount of days
-but if the client books the vehicle for > 7 days a discount of 20% will be applicable and will be automatically cut from the price.
+but if the client books the vehicle for > 7 days a discount of 20% will be applicable and 
+will be automatically cut from the price.
 
 **Address**
 Stores the address of clients and bases, has a one-to-one relationship with client
@@ -151,7 +148,8 @@ This the entity that holds the additional drivers from the booking.
 Therefore, it has many to one relationship with the booking.
 
 **Client**
-It holds the information of a client. has a one to one relation with address and one to many relation with booking
+It holds the information of a client. has a one to one relation with address 
+and one to many relation with booking
 
 
 **Bind**
@@ -165,3 +163,39 @@ but below 30 the car types they are allowed to take is restricted whereas if the
 they can take any car type.
 */
 
+/*
+T4 Indexes (With Comments)
+*/
+CREATE INDEX EmployeeID ON Employee(FirstName, LastName, BaseID);
+CREATE INDEX BookingDetails ON Booking(SSN, VehicleID, Day);
+CREATE INDEX VehicleID ON Vehicle(Name, Type, Availability, Cost);
+CREATE INDEX ClientDetails ON Client(FirstName,LastName,Age,Mobile,Email);
+CREATE INDEX Address ON Address(StreetName, City, PostCode);
+
+/*
+**EmployeeID**
+As the company HW Motors has many bases across UK and are a company focused on expanding
+the company would have a number of employees with similar first names and last names
+an index would be useful on th employee's (FirstName, LastName, BaseID) to make it 
+easier to find the employees in the company.
+
+**BookingDetails**
+HWMotors as an expanding comnpany would have a lot of Bookings with the same client
+so to narrow it down and make it easier to search having an index on (SSN, VehicleID, Day)
+would make it easier to find the specific booking
+
+**VehicleID**
+Would make it easier to find if a specific vehicle is available and if the vehicle type is 
+suited for the client.
+
+**ClientDetails**
+Similar to EmployeeID as the company is an expanding company with good aims the company is bound
+to have many clients with similar names and DOBs. An index on Client would provide the ability to
+search for the details of a client more efficently and faster.
+
+**Address**
+Address is an entity which contains the address for all the companies clients and bases as these 
+attributes can have a multitude of enteries having an index here could make it easier to 
+search or a specific address using(StreetName, City, PostCode).
+
+*/
